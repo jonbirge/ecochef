37e7a27f44939d6a03074051a26408a186c1ad32
Jonathan Birge <birge@mit.edu>
2017-06-09 21:47:12 -0400

    Initial Commit

---
+++ EcoChef/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  EcoChef
+//
+//  Created by Jonathan Birge on 6/9/17.
+//  Copyright © 2017 Birge Clocks. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
