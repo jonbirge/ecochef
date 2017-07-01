ad072a855570f8631857776ecef7fb7c21d2d8bc
Jonathan Birge <birge@mit.edu>
2017-06-30 22:31:37 -0400

    refactor state class into new file

--- EcoChef/PreheatViewController.swift
+++ EcoChef/PreheatViewController.swift
@@ -9,38 +9,6 @@
 import UIKit
 import UserNotifications
 
-class EcoChefState : NSObject, NSCoding {
-    var Tamb: Float
-    var selectedModel: Int
-    
-    struct PropertyKeys {
-        static let Tamb = "tamb"
-        static let selectedModel = "selectedmodel"
-    }
-    
-    override init() {
-        self.Tamb = 70
-        self.selectedModel = 0
-        super.init()
-    }
-    
-    init(Tamb: Float, selectedModel: Int) {
-        self.Tamb = Tamb
-        self.selectedModel = selectedModel
-    }
-    
-    required convenience init(coder aDecoder: NSCoder) {
-        let Tamb = aDecoder.decodeFloat(forKey: PropertyKeys.Tamb)
-        let selectedModel = aDecoder.decodeInteger(forKey: PropertyKeys.selectedModel)
-        self.init(Tamb: Tamb, selectedModel: selectedModel)
-    }
-    
-    func encode(with aCoder: NSCoder) {
-        aCoder.encode(Tamb, forKey: PropertyKeys.Tamb)
-        aCoder.encode(selectedModel, forKey: PropertyKeys.selectedModel)
-    }
-}
-
 class PreheatViewController : UIViewController, UNUserNotificationCenterDelegate {
     let smallstep: Float = 2
     let largestep: Float = 25
