ed55c75647d4525fbc41edfbe17c04c6412d655d
Jonathan Birge <birge@mit.edu>
2017-07-02 23:30:17 -0400

    update custom button to outline

--- EcoChef/TimerButton.swift
+++ EcoChef/TimerButton.swift
@@ -7,32 +7,33 @@ import UIKit
 @IBDesignable
 
 class TimerButton: SimpleButton {
+    let lightgrey = UIColor(white: 0.9, alpha: 1)
     
     /// Background color for normal state
-    @IBInspectable var normalBackgroundColor: UIColor?
-    @IBInspectable var highlightBackgroundColor: UIColor?
-    @IBInspectable var titleColorNormal: UIColor?
     @IBInspectable var cornerRadius: CGFloat = 5
+    @IBInspectable var titleColorNormal: UIColor?
     
     override func configureButtonStyles() {
         super.configureButtonStyles()
         
         setCornerRadius(cornerRadius)
         
-        if let backgroundColorNormal = normalBackgroundColor {
-            setBackgroundColor(backgroundColorNormal, for: .normal)
-        }
-        if let backgroundColorHighlight = highlightBackgroundColor {
-            setBackgroundColor(backgroundColorHighlight, for: .highlighted)
+        if let normalColor = titleColorNormal {
+            setBackgroundColor(.white, for: .normal)
+            setBorderColor(normalColor, for: .normal)
+            setTitleColor(normalColor, for: .normal)
+            var hue: CGFloat = 0
+            var sat: CGFloat = 0
+            var brightness: CGFloat = 0
+            normalColor.getHue(&hue, saturation: &sat, brightness: &brightness, alpha: nil)
+            let highlightbrightness: CGFloat = 1 - (1 - brightness)/3
+            let highlightColor = UIColor(hue: hue, saturation: sat, brightness: highlightbrightness, alpha: 1)
+            setTitleColor(highlightColor, for: .highlighted)
         }
         
-        let lightgrey = UIColor(white: 0.9, alpha: 1)
-        setBackgroundColor(lightgrey, for: .disabled, animated: true, animationDuration: 0.1)
-        setTitleColor(UIColor.white, for: .disabled)
-        
-        if let titleColorNormal = titleColorNormal {
-            setTitleColor(titleColorNormal, for: .normal)
-        }
+        setBorderWidth(1.0)
+        setBorderColor(lightgrey, for: .disabled, animated: true, animationDuration: 0.1)
+        setTitleColor(lightgrey, for: .disabled)
     }
     
 }
