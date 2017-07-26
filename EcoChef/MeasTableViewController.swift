7b655eb7d5aec5353e74a31c01561e97ea32d48e
Jonathan Birge <birge@mit.edu>
2017-07-26 01:45:39 -0400

    allow measurements to be deleted

--- EcoChef/MeasTableViewController.swift
+++ EcoChef/MeasTableViewController.swift
@@ -41,6 +41,15 @@ class MeasTableViewController: UITableViewController {
         return cell
     }
     
+    override func tableView(_ tableView: UITableView,
+                            commit editingStyle: UITableViewCellEditingStyle,
+                            forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            measData.measlist.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
+    }
+    
     // MARK: - Navigation
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
